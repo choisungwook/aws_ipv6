86ea65e9fc49193513b57bb7f9719989ee422cfc
choisungwook <kgg1959@naver.com>
2024-02-25 16:57:07 +0900

    KAN-121 Add auto_public_ipc at ipv6 public subnet

--- example1/_module/ipv6/subnets.tf
+++ example1/_module/ipv6/subnets.tf
@@ -2,9 +2,13 @@ resource "aws_subnet" "public" {
   for_each = var.public_subnets
 
   vpc_id            = var.vpc_id
-  cidr_block        = each.value["ipv4_cidr"]
   availability_zone = each.value["az"]
 
+  // IPv4
+  cidr_block              = each.value["ipv4_cidr"]
+  map_public_ip_on_launch = true
+
+  // IPv6
   assign_ipv6_address_on_creation = true
   ipv6_cidr_block                 = cidrsubnet(var.vpc_ipv6_cidr, 8, each.value["ipv6_subnet_netnum"])
 
@@ -16,9 +20,14 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   for_each = var.private_subnets
 
-  vpc_id                          = var.vpc_id
-  availability_zone               = each.value["az"]
-  cidr_block                      = each.value["ipv4_cidr"]
+  vpc_id            = var.vpc_id
+  availability_zone = each.value["az"]
+
+  // IPv4
+  cidr_block              = each.value["ipv4_cidr"]
+  map_public_ip_on_launch = true
+
+  // IPv6
   assign_ipv6_address_on_creation = true
   ipv6_cidr_block                 = cidrsubnet(var.vpc_ipv6_cidr, 8, each.value["ipv6_subnet_netnum"])
 
