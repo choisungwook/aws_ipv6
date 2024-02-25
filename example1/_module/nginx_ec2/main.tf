58bdc3961a81a5150da09f59afbfa37aca470deb
choisungwook <kgg1959@naver.com>
2024-02-25 11:18:13 +0900

    Add user_data at spot instance

--- example1/_module/nginx_ec2/main.tf
+++ example1/_module/nginx_ec2/main.tf
@@ -4,6 +4,7 @@ resource "aws_spot_instance_request" "nginx" {
   instance_type        = var.spot_instance_type
   spot_type            = var.spot_type
   wait_for_fulfillment = "true"
+  user_data            = file("${path.module}/userdata.sh")
   security_groups      = [aws_security_group.nginx.id]
   subnet_id            = var.subnet_id
 
