d646d0d22177c4a14eb4b8402105ef0b64f3079e
choisungwook <kgg1959@naver.com>
2024-03-17 14:03:43 +0900

    Change Python httpserver AMI_id (#30)

--- _module/dualstack_http_sevrer/data.tf
+++ _module/dualstack_http_sevrer/data.tf
@@ -1,28 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
