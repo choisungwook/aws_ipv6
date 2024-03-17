e0ee080c9cf5b6ca8001a7df21c3e5e1b6654b84
choisungwook <kgg1959@naver.com>
2024-03-17 13:35:47 +0900

    Change Python httpserver instance type (#29)

--- _module/ipv6_only_http_server/ec2.tf
+++ _module/ipv6_only_http_server/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "http-server" {
-  instance_type          = "t4g.nano"
+  instance_type          = "t4g.medium"
   ami                    = "ami-0baf474c04c731d22" # or data.aws_ami.ubuntu.id
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.httpserver-ipv6.id]
